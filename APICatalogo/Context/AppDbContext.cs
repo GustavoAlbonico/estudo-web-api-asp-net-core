921a506eea2eb0afa28089dc4ec27bb5daaedfec
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-22 16:15:44 -0300

    feat - criando projeto e adicionando conexão com o banco de dados

---
+++ APICatalogo/Context/AppDbContext.cs
@@ -0,0 +1,10 @@
+using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+}
---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,16 @@
+namespace APICatalogo.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string? UrlImagem { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
+
+}
