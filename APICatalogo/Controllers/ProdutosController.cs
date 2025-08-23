f8d87ba923e7c342b7d8aaa78f6ff895197e1b38
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-23 17:30:44 -0300

    feat -  adicionando GET | POST

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,49 @@
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APICatalogo.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProdutosController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> Get()
+    {
+        var produtos = _context.Produtos.ToList();
+
+        if (produtos is null)
+            return NotFound("Produtos não encontrado...");
+
+        return produtos;
+    }
+
+    [HttpGet("{id:int}", Name = "ObterProduto")]
+    public ActionResult<Produto> Get(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.Id == id);
+
+        if (produto is null)
+            return NotFound("Produtos não encontrado...");
+
+        return produto;
+    }
+
+    public ActionResult Post(Produto produto)
+    {
+        if (produto is null)
+            return BadRequest();
+
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+
+        return new CreatedAtRouteResult("ObterProduto", new { id = produto.Id }, produto);
+    }
+}
