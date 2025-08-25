e72e3f1e70e26e0aa49355abc5f653cd1320e3b3
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-25 11:13:06 -0300

    feat - ajustes na serialização e deserialização do json

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,10 +1,14 @@
+using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers()
+                .AddJsonOptions(options =>
+                    options.JsonSerializerOptions
+                        .ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
-builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
