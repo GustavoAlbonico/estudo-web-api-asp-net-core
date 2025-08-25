d2b39e36634345ec2ca2880a03c7a705b99002c4
Usuario <gustavoalbonicogoncalves@hotmail.com>
2025-08-25 11:08:18 -0300

    feat - Produto --> Put | Delete

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers;
 
@@ -20,7 +21,7 @@ public class ProdutosController : ControllerBase
         var produtos = _context.Produtos.ToList();
 
         if (produtos is null)
-            return NotFound("Produtos não encontrado...");
+            return NotFound("Produto não encontrado...");
 
         return produtos;
     }
@@ -31,11 +32,12 @@ public class ProdutosController : ControllerBase
         var produto = _context.Produtos.FirstOrDefault(p => p.Id == id);
 
         if (produto is null)
-            return NotFound("Produtos não encontrado...");
+            return NotFound("Produto não encontrado...");
 
         return produto;
     }
 
+    [HttpPost]
     public ActionResult Post(Produto produto)
     {
         if (produto is null)
@@ -45,5 +47,32 @@ public class ProdutosController : ControllerBase
         _context.SaveChanges();
 
         return new CreatedAtRouteResult("ObterProduto", new { id = produto.Id }, produto);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Produto produto)
+    {
+        if (id != produto.Id)
+            return BadRequest();
+
+        _context.Entry(produto).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
+
+    [HttpDelete("{id:int}")]
+    public ActionResult Delete(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.Id == id);
+
+        if (produto is null)
+            return NotFound("Produto não localizado...");
+
+
+        _context.Produtos.Remove(produto);
+        _context.SaveChanges();
+
+        return Ok(produto);
     }
 }
