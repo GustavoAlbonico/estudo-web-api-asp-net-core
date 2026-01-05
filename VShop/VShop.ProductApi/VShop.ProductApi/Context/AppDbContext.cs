ef6cb511092e44f2a0bcd64a1084a05337285d47
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2026-01-05 19:57:17 -0300

    feat - VShopProdutosApi adicionando models e migrations

---
+++ VShop/VShop.ProductApi/VShop.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Context;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>()
+            .HasKey(c => c.Id);
+
+        modelBuilder.Entity<Category>()
+            .Property(c => c.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+        
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Name)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Description)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.ImageUrl)
+            .HasMaxLength(255)
+            .IsRequired();
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Price)
+            .HasPrecision(12, 2);
+
+        modelBuilder.Entity<Category>()
+            .HasMany(p => p.Products)
+            .WithOne(c => c.Category)
+            .IsRequired()
+            .OnDelete(DeleteBehavior.Cascade);
+
+
+        modelBuilder.Entity<Category>().HasData(
+            new
+            {
+                Id = 1,
+                Name = "Material Escolar",
+            },
+            new
+            {
+                Id = 2,
+                Name = "Acessórios",
+            }
+        );
+    }
+
+}
--- VShop/VShop.ProductApi/VShop.ProductApi/Program.cs
+++ VShop/VShop.ProductApi/VShop.ProductApi/Program.cs
@@ -1,17 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using VShop.ProductApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "VShop ProductApi", Version = "v1" });
+});
+
+string? mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection))
+           //.UseSnakeCaseNamingConvention()
+);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "VShop ProductApi");
+    });
 }
 
 app.UseHttpsRedirection();
