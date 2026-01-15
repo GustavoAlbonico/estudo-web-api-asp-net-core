5d2d20083e5695b34c76a73c75201d02e205e6da
GustavoAlbonico <gustavoalbonicogoncalves@hotmail.com>
2026-01-15 20:14:59 -0300

    feat - VShop.Web adicionando view de login, e 'endpoint' para login e logout

--- VShop/VShop.Web/Controllers/HomeController.cs
+++ VShop/VShop.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-using System.Diagnostics;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 using VShop.Web.Models;
 
 namespace VShop.Web.Controllers
@@ -27,6 +29,18 @@ namespace VShop.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> Login()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
         }
     }
 }
