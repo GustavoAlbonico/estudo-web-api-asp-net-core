cc8c8d38c6b2ace600a89b7ed4f9de7e450b02ca
Usuario <gustavoalbonicogoncalves@hotmail.com>
2026-01-20 21:56:06 -0300

    feat - VShop.DiscountApi adicionando model e dto de desconto

--- VShop/VShop.DiscountApi/DTOs/CouponDTO.cs
+++ VShop/VShop.DiscountApi/DTOs/CouponDTO.cs
@@ -1,5 +1,12 @@
-﻿namespace VShop.DiscountApi.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VShop.DiscountApi.DTOs;
 
 public class CouponDTO
 {
+    public int Id { get; set; }
+    [Required]
+    public string? CouponCode { get; set; }
+    [Required]
+    public decimal Discount { get; set; }
 }
--- VShop/VShop.DiscountApi/Models/Coupon.cs
+++ VShop/VShop.DiscountApi/Models/Coupon.cs
@@ -1,5 +1,13 @@
-﻿namespace VShop.DiscountApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VShop.DiscountApi.Models;
 
 public class Coupon
 {
+    public int Id { get; set; }
+
+    public string? CouponCode { get; set; }
+   
+    public decimal Discount { get; set; }
 }
